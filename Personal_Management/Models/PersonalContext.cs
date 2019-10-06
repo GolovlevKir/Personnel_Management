79c23d82ed1ae1b8d2f23531cc2c2555f7d9ad8f
кирилл головлев <kirvik12122000@gmail.com>
2019-10-06 14:17:52 +0300

    Создание контекста для доступа к БД на основе модели. Создание класа со стракой подключения

---
+++ Personal_Management/Models/PersonalContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Personal_Management.Models
+{
+    //использует EntityFramework для доступа к БД на основе некоторой модели
+    public class PersonalContext : DbContext
+    {
+        public DbSet<Account> Account { get; set; }
+        public DbSet<Departments> Departments { get; set; }
+        public DbSet<Documents> Documents { get; set; }
+        public DbSet<Isp_Sroki> Isp_Sroki { get; set; }
+        public DbSet<Pass_Dannie> Pass_Dannie { get; set; }
+        public DbSet<Positions> Positions { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Rates> Rates { get; set; }
+        public DbSet<Sbor_Docum> Sbor_Docum { get; set; }
+        public DbSet<Sotrs> Sotrs { get; set; }
+        public DbSet<status_isp_sroka> status_isp_sroka { get; set; }
+        public DbSet<Work_Schedule> Work_Schedule { get; set; }
+        public DbSet<Zar_Plata> Zar_Plata { get; set; }
+    }
+}
---
+++ Personal_Management/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Personal_Management
+{
+    public class Program
+    {
+        public static int id;
+        public static SqlConnection SqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["PersonalContext"].ToString());
+    }
+}
